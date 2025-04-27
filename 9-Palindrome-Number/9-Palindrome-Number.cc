10fd7a49d4c74e62efb5b23b30231cdd890a9089
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-27 11:59:22 +0100

    Solve Palindrome-Number problem

---
+++ 9-Palindrome-Number/9-Palindrome-Number.cc
@@ -0,0 +1,29 @@
+
+/**************************************************************************************************************************
+ * Execution Time : 0 ms (leetcode test)
+ **************************************************************************************************************************/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        int reverse = this->reverse(x);
+        return x == reverse;
+    }
+
+    int reverse(int x) {
+        int sign = (x > 0) ? 1: -1;
+        long int value = (long int)x * sign;
+
+        long int a = 10;
+        long int newX = 0;
+        while((a/10) <= value){
+            newX = (newX * 10) + ((value % a)/ (a/10));
+            a*=10;
+        }
+        newX = newX * sign;
+        if(newX < -2147483648 || newX > 2147483647) return 0;
+
+        return newX;
+    }
+};
